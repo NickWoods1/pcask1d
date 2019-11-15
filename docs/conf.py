401b559d18eecd88489cb9ee1d34b23e7cdd9e82
Nick <nw361@cam.ac.uk>
2019-11-15 13:24:47 +0000

    Docs...

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,8 @@ author = 'Nick Woods'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
+master_doc = 'index'
+
 
 # -- General configuration ---------------------------------------------------
 
